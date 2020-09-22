69235cca0c32f4c7a68f20f07f85aa2aa58d5183
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-09-22 20:24:51 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,12 +9,13 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  quotient = num1 / num2
+  quotient = num2 / num1
   puts quotient
 end
 
 def multiplication(num1, num2)
-  
+  product = num1 * num2
+  puts product
 end
 
 def modulo(num1, num2)
